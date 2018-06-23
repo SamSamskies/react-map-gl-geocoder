daa0985819a94429862e55873a12829734104ba9
Steven Miyakawa <steven.miyakawa@here.com>
2018-06-22 17:00:26 -0700

    First draft

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
+import { Component } from 'react'
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder'
+import { getAccessToken } from 'react-map-gl/dist/mapbox/mapbox'
+import PropTypes from 'prop-types'
+
+class Geocoder extends Component {
+  componentDidUpdate() {
+    if (this.geocoder !== undefined) {
+      return
+    }
+
+    const { mapRef, mapboxApiAccessToken, options } = this.props
+    const map = mapRef.current.getMap()
+
+    this.geocoder = map.addControl(new MapboxGeocoder({ accessToken: mapboxApiAccessToken, ...options }))
+  }
+
+  getGeocoder() {
+    return this.geocoder
+  }
+
+  render() {
+    return null
+  }
+
+  static propTypes = {
+    mapRef: PropTypes.object.isRequired,
+    mapboxApiAccessToken: PropTypes.string,
+    options: PropTypes.object
+  }
+
+  static defaultProps = {
+    mapboxApiAccessToken: getAccessToken()
+  }
+}
+
+export default Geocoder
